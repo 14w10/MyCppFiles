dd669cb0317274cf4fe43599a23a8dba27708ade
DSAERF-CALMIT <z2047ds@gmail.com>
2021-12-18 03:54:38 -0800

    Create main.cpp

---
+++ Leetcode/0168Excel/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string convertToTitle(int n) {
+        vector<char>rst;
+        string rs;
+        while (n > 0) {
+            n -= 1;
+            rst.push_back('A' + (n % 26));
+            n /= 26;
+        }
+        reverse(rst.begin(), rst.end());
+        rs.assign(rst.begin(), rst.end());
+        return rs;
+    }
+};
+int main(int argc, const char * argv[]) {
+    Solution s;
+    cout << s.convertToTitle(701) << endl;
+    return 0;
+}
