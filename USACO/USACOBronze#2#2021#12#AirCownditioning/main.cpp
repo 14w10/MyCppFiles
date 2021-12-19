568d7104102946c7f41de15a3238e0effbfa5e86
DSAERF-CALMIT <z2047ds@gmail.com>
2021-12-19 00:20:43 -0800

    Update main.cpp

--- USACO/USACOBronze#2#2021#12#AirCownditioning/main.cpp
+++ USACO/USACOBronze#2#2021#12#AirCownditioning/main.cpp
@@ -17,9 +17,11 @@ int main()
     int gra = 0, ans = 0;
     for (int i = 0; i < n; i++) {
         if (cha[i] * gra >= 0){
+            // if(Both Increase Temperature || Both Decrease Temperature)
             ans += max(abs(cha[i]) - abs(gra), 0);
         }
         else{
+            // if(Increase --> Decrease || Decrease --> Increase)
             ans += abs(cha[i]);
         }
         gra = cha[i];
